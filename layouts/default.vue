2ce515d0e2a7b76e7149a49cb2fadbfd60044dd1
Eyumul <eyuelmulugeta60@gmail.com>
2025-04-22 17:37:51 +0300

    + create a default layout with nav section and header section + add sensor data cards + add dummy sensor information

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <slot/>
+    <div class="min-h-dvh bg-background p-[64px] flex justify-between gap-[71px]">
+        <div class="w-[15%] flex flex-col gap-[36px] pt-[90px]">
+            <div class="bg-violet-2 rounded-[72px] w-full flex gap-[14px] text-violet-1 font-bold text-lg pl-14 items-center py-[18px]">
+                <icon name="mdi:chart-pie"/>
+                <p>Data Stats</p>
+            </div>
+            <div class="bg-white rounded-[72px] w-full flex gap-[14px] text-[#D0D2DA] hover:text-violet-1 cursor-pointer font-bold text-lg pl-14 items-center py-[18px]">
+                <icon name="material-symbols:bolt-rounded"/>
+                <p>Power Usage</p>
+            </div>
+            <div class="h-px rounded-full bg-[#D0D2DA] mx-6"></div>
+            <div class="w-full flex gap-[14px] text-[#92959E] hover:text-violet-1 cursor-pointer font-bold text-lg justify-center items-center py-[18px]">
+                <icon name="ion:log-out"/>
+                <p>Logout</p>
+            </div>
+        </div>
+        <div class="w-[85%] flex flex-col gap-[35px]">
+            <div class="w-full h-[55px]">
+                <p class="text-4xl font-medium">Data Stats</p>
+                <p class="text-lg text-[#92959E] mt-[4px]">Information about Sensors Reading</p>
+            </div>
+            <div class="w-full flex-1">
+                <slot/>
+            </div>
+        </div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,75 @@
 <template>
-    <div>
-        <h1 class="text-5xl text-red-500 font-black">Hello world</h1>
-        <icon name="uil:0-plus"/>
+    <div class="flex gap-[71px] h-full">
+        <div class="flex flex-col gap-[25px] w-[75%] h-full">
+            <div class="flex items-center justify-center gap-[28px] bg-white rounded-[14px] shadow-md w-full h-1/4">
+                <template v-for="(sensor, index) in sensorsData" :key="sensor.number">
+                    <SensorCard
+                        :number="sensor.number"
+                        :amount="sensor.amount"
+                        :cardColor="sensor.cardColor"
+                    />
+                    <!-- Add divider between cards except after the last one -->
+                    <div
+                        v-if="index < sensorsData.length - 1"
+                        class="w-px h-4/5 rounded-full bg-[#D0D2DA]"
+                    ></div>
+                </template>
+            </div>
+            <div class="bg-white rounded-[14px] shadow-md w-full h-3/4"></div>
+        </div>
+        <div class="flex flex-col gap-[21px] bg-white p-[24px] rounded-[14px] shadow-md w-[25%] h-full">
+            <p class="text-xl mb-8">Details</p>
+            <template v-for="(sensor, index) in sensorsData" :key="sensor.number">
+                <SensorDetailCard
+                    :number="sensor.number"
+                    :amount="sensor.amount"
+                    :cardColor="sensor.cardColor"
+                    :sensorName="sensor.sensorName"
+                    :physicalQuantity="sensor.physicalQuantity"
+                    :unit="sensor.unit"
+                />
+                <!-- Add divider between cards except after the last one -->
+                <div 
+                    v-if="index < sensorsData.length - 1"
+                    class="h-px rounded-full bg-[#D0D2DA]"
+                ></div>
+            </template>
+        </div>
     </div>
 </template>
+<script setup>
+const sensorsData = [
+    {
+        number: 1,
+        amount: 45,
+        cardColor: "orange",
+        sensorName: "Moisture sensor",
+        physicalQuantity: "Soil moisture",
+        unit: "%"
+    },
+    {
+        number: 2,
+        amount: 30,
+        cardColor: "violet",
+        sensorName: "Moisture sensor",
+        physicalQuantity: "Humidity",
+        unit: "g/m³"
+    },
+    {
+        number: 3,
+        amount: 15,
+        cardColor: "blue",
+        sensorName: "Temprature sensor",
+        physicalQuantity: "Soil tempratue",
+        unit: "℃"
+    },
+    {
+        number: 4,
+        amount: 15,
+        cardColor: "red",
+        sensorName: "Ultrasonic sensor",
+        physicalQuantity: "Distance",
+        unit: "cm"
+    },
+]
+</script>
