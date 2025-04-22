64900adf7473c66087e2253096df97e40bb74672
Eyumul <eyuelmulugeta60@gmail.com>
2025-04-22 17:34:58 +0300

    + create mini sensor cards + create detailed sensor card

---
+++ components/sensor-card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex gap-[8px] h-fit">
+        <div class="rounded-[11px] flex justify-center items-center font-bold size-[80px] text-3xl" :class="`text-${cardColor}-1 bg-${cardColor}-2`">{{ number }}</div>
+        <div class="flex flex-col justify-between p-2">
+            <p class="text-[#92959E]">{{ sensorLabel }}</p>
+            <p class="text-2xl">{{ amount }}</p>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps([
+    'number',
+    'amount',
+    'cardColor'
+])
+const sensorLabel = computed(() => {
+  const words = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+  return `Sensor ${words[props.number - 1]}`
+})
+</script>
---
+++ components/sensor-detail-card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex justify-between">
+        <div class="flex flex-col gap-2">
+            <p :class="`text-${cardColor}-1`">{{ sensorLabel }} <span class="text-black">({{ sensorName }})</span></p>
+            <div class="flex flex-col gap-2 font-bold">
+                <p>{{ physicalQuantity }}: <span :class="`text-${cardColor}-1 text-2xl font-bold`">{{ amount }}{{ unit }}</span></p>
+            </div>
+        </div>
+        <p :class="`text-${cardColor}-1 font-medium`">{{ unit }}</p>
+    </div>
+</template>
+<script setup>
+const props = defineProps([
+    'number',
+    'sensorName',
+    'physicalQuantity',
+    'cardColor',
+    'unit',
+    'amount'
+])
+const sensorLabel = computed(() => {
+  const words = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+  return `Sensor ${words[props.number - 1]}`
+})
+</script>
